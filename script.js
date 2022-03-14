187e0ea38bc43aafbded5a4d0825ecdab458141f
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-03-14 18:57:45 +0000

    Added Pen Tool
    Made the pen tool workable with JS

--- script.js
+++ script.js
@@ -14,7 +14,32 @@ window.addEventListener("load",()=>{
        
     })
     
-    
+    let painting = false;
+
+    function startPosition (e){
+        painting = true;
+        draw(e);
+    }
+
+    function finishedPosition(){
+        painting = false;
+        ctx.beginPath();
+    }
+
+    function draw(e){
+        if(!painting) return;
+        ctx.lineWidth = 5;
+        ctx.lineCap = "round";
+        ctx.strokeStyle = "red";
+
+        ctx.lineTo(e.clientX , e.clientY - tools.offsetHeight);
+        ctx.stroke();
+        // ctx.beginPath()
+        ctx.moveTo(e.clientX, e.clixentY - tools.offsetHeight)
+    }
+    canvas.addEventListener("mousedown", startPosition)
+    canvas.addEventListener("mouseup", finishedPosition)
+    canvas.addEventListener("mousemove", draw)
     
    
 })
